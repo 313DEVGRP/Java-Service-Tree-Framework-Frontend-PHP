2875602d3e3559e3cec4667059a26ecda9f7a06f
LeeDongMin <admin@313.co.kr>
2023-11-21 11:18:36 +0900

    feat: [ARMSQA-19] #comment :white_check_mark: 313DEVGRP 테마 만들기 : 기존 커뮤니티 코드 옮겨오기 - 23.11.21 #close #time 1h +review SR @313devops
    [ Frontend-PHP::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    요구 관리 : [Requirement Manage] http://www.a-rms.net
    문서 관리 : [Document] http://www.313.co.kr/confluence
    이슈 관리 : [IssueTracker] http://www.313.co.kr/jira
    소스 관리 : [VersionControl] https://github.com/313devgrp
    코드 리뷰 : [CodeReview] http://www.313.co.kr/fecru
    빌드 관리 : [CICD-Deploy] http://www.313.co.kr/jenkins
    배포 관리 : [Deploy-Pipeline] http://www.313.co.kr/spinnaker
    아티 팩트 : [ArtifactManager] http://www.313.co.kr/nexus
    코드 분석 : [CodeAnalysis] http://www.313.co.kr/sonar
    성능 측정 : [ZipKin] http://www.313.co.kr/zipkin/
    
    도커 관리 : [Docker Hub] https://hub.docker.com/u/313devgrp
    클러스터1 : [Kubernetes] http://www.313.co.kr:31380
    클러스터2 : [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    디비 관리 : [DB Admin] http://www.313.co.kr/phpmyadmin/
    오브 젝트 : [S3 Admin] http://www.313.co.kr/minio/login
    캐시 관리 : [Cache Admin] http://www.313.co.kr/redis/
    ELFK 스택1 : [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    ELFK 스택2 : [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    ELFK 스택3 : [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    ELFK 스택4 : [Kibana] http://www.313.co.kr/kibana/app/home#/
    
    모니터링 1 : [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    모니터링 2 : [Grafana] http://www.313.co.kr/grafana
    
    인증 인가 : [Auth] http://www.313.co.kr/auth/
    
    NFS 관리 : [NAS] http://www.313.co.kr:5000/webman/index.cgi
    메일 관리 : [Mail] http://www.313.co.kr/mail/
    원격 관리 : [RDP] http://www.313.co.kr/guacamole/#/

--- theme/313devgrp/index.php
+++ theme/313devgrp/index.php
@@ -1,72 +1,253 @@
-<?php
-if (!defined('_INDEX_')) define('_INDEX_', true);
-if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+<!DOCTYPE html>
+<html>
+<head>
+    <title>313DEVGRP Community</title>
 
-if (G5_IS_MOBILE) {
-    include_once(G5_THEME_MOBILE_PATH.'/index.php');
-    return;
-}
+    <meta   charset="utf-8" />
+    <meta   http-equiv="X-UA-Compatible"
+            content="IE=edge" />
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta   name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
 
-if(G5_COMMUNITY_USE === false) {
-    include_once(G5_THEME_SHOP_PATH.'/index.php');
-    return;
-}
+    <meta   http-equiv="Expire"
+            content="-1" />
+    <meta   http-equiv="Keywords"
+            content="313 DEV GRP" />
+    <meta   http-equiv="Reply-to"
+            content="313cokr@gmail.com" />
+    <meta   http-equiv="Content-Language"
+            content="Korean" />
+    <meta   http-equiv="Last-Modified"
+            content="Thu 01 Nov 2016 23:59:59" />
+    <meta   http-equiv="Organization"
+            content="www.313.co.kr" />
+    <meta   http-equiv="Content-Type"
+            content="text/html;charset=utf-8" />
+    <meta   http-equiv="Cache-Control"
+            content="no-cache" />
+    <meta   http-equiv="Pragma"
+            content="no-cache" />
+    <meta   http-equiv="imagetoolbar"
+            content="no" />
+    <meta   http-equiv="content-Script-type"
+            content="text/javascript" />
+    <meta   http-equiv="content-Style-type"
+            content="text/css" />
 
-include_once(G5_THEME_PATH.'/head.php');
-?>
+    <meta   name="robots"
+            content="ALL, INDEX, FOLLOW" />
+    <meta   name="Subject"
+            content="313 DEV GRP" />
+    <meta   name="Filename"
+            content="index.html" />
+    <meta   name="Author-Date"
+            content="31 Oct 16" />
+    <meta   name="Date"
+            content="31 Oct 16" />
+    <meta   name="Author"
+            content="LeeDongmin" />
+    <meta   name="Other Agent"
+            content="LeeDongMin" />
+    <meta   name="Email"
+            content="313cokr@gmail.com" />
+    <meta   name="Reply-To"
+            content="313cokr@gmail.com" />
+    <meta   name="Project"
+            content="Java Service Tree Framework" />
+    <meta   name="Status"
+            content="Draft" />
+    <meta   name="Location"
+            content="South Korea" />
+    <meta   name="distribution"
+            content="313 DEV GRP" />
+    <meta   name="Description"
+            content="Standard Web Project" />
+    <meta   name="verify-v1"
+            content="Eal6+fiCjgKAZb5A6pRvSLmsh9NLF2AsqxqJrLuFoAs=" />
+    <meta   name="Revision"
+            content="1.0" />
+    <meta   name="Generator"
+            content="eclipse 3.6.1" />
+    <meta   name="Classification"
+            content="Development,Deployment" />
+    <meta   name="Copyright"
+            content="CopyRight@aRMS. All Rights Reserved" />
+    <meta   name="title"
+            content="313 DEV GRP" />
+    <meta   name="revisit-after"
+            content="7 days" />
+    <meta   name="siteinfo"
+            content="http://www.313.co.kr/robots.txt" />
+    <meta   name="HandheldFriendly"
+            content="True" />
+    <meta   name="MobileOptimized"
+            content="320" />
 
-<h2 class="sound_only">최신글</h2>
+    <meta   property="og:type"
+            content="website" />
+    <meta   property="og:title"
+            content="313 DEV GRP" />
+    <meta   property="og:url"
+            content="http://www.313.co.kr/" />
+    <meta   property="og:site_name"
+            content="313 DEV GRP" />
 
-<div class="latest_top_wr">
-    <?php
-    // 이 함수가 바로 최신글을 추출하는 역할을 합니다.
-    // 사용방법 : latest(스킨, 게시판아이디, 출력라인, 글자수);
-    // 테마의 스킨을 사용하려면 theme/basic 과 같이 지정
-    echo latest('theme/pic_list', 'free', 4, 23);		// 최소설치시 자동생성되는 자유게시판
-	echo latest('theme/pic_list', 'qa', 4, 23);			// 최소설치시 자동생성되는 질문답변게시판
-	echo latest('theme/pic_list', 'notice', 4, 23);		// 최소설치시 자동생성되는 공지사항게시판
-    ?>
-</div>
-<div class="latest_wr">
-    <!-- 사진 최신글2 { -->
-    <?php
-    // 이 함수가 바로 최신글을 추출하는 역할을 합니다.
-    // 사용방법 : latest(스킨, 게시판아이디, 출력라인, 글자수);
-    // 테마의 스킨을 사용하려면 theme/basic 과 같이 지정
-    echo latest('theme/pic_block', 'gallery', 4, 23);		// 최소설치시 자동생성되는 갤러리게시판
-    ?>
-    <!-- } 사진 최신글2 끝 -->
-</div>
+    <!-- favicon -->
+    <link   rel="icon"
+            href="./img/313_logo.ico"
+            type="image/x-icon" />
+    <link   rel="shortcut icon"
+            href="./img/313_logo.ico"
+            type="image/x-icon" />
 
-<div class="latest_wr">
-<!-- 최신글 시작 { -->
-    <?php
-    //  최신글
-    $sql = " select bo_table
-                from `{$g5['board_table']}` a left join `{$g5['group_table']}` b on (a.gr_id=b.gr_id)
-                where a.bo_device <> 'mobile' ";
-    if(!$is_admin)
-	$sql .= " and a.bo_use_cert = '' ";
-    $sql .= " and a.bo_table not in ('notice', 'gallery') ";     //공지사항과 갤러리 게시판은 제외
-    $sql .= " order by b.gr_order, a.bo_order ";
-    $result = sql_query($sql);
-    for ($i=0; $row=sql_fetch_array($result); $i++) {
-		$lt_style = '';
-    	if ($i%3 !== 0 ) $lt_style = "margin-left:2%";
-    ?>
-    <div style="float:left;<?php echo $lt_style ?>" class="lt_wr">
-        <?php
-        // 이 함수가 바로 최신글을 추출하는 역할을 합니다.
-        // 사용방법 : latest(스킨, 게시판아이디, 출력라인, 글자수);
-        // 테마의 스킨을 사용하려면 theme/basic 과 같이 지정
-        echo latest('theme/basic', $row['bo_table'], 6, 24);
-        ?>
+    <link   href="/313devgrp/reference/lightblue4/docs/css/application.min.css"
+            rel="stylesheet" />
+    <link   href="/313devgrp/arms/css/override.css"
+            rel="stylesheet" />
+
+    <!-- common libraries. required for every page-->
+    <script type="text/javascript" src="/313devgrp/reference/lightblue4/docs/lib/jquery/dist/jquery.min.js"></script>
+    <script type="text/javascript" src="/313devgrp/reference/lightblue4/docs/lib/jquery-ui/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="/313devgrp/reference/jquery-plugins/jquery-cookie-1.4.1/jquery.cookie.js"></script>
+    <script type="text/javascript" src="/313devgrp/reference/lightblue4/docs/lib/bootstrap-sass/assets/javascripts/bootstrap.min.js"></script>
+
+    <script type="text/javascript" src="/313devgrp/reference/jquery-plugins/jnotify_v2.1/jquery/jNotify.jquery.min.js" charset="utf-8"></script>
+    <link rel="stylesheet" type="text/css" href="/313devgrp/reference/jquery-plugins/jnotify_v2.1/jquery/jNotify.jquery.css" media="screen" />
+
+    <!-- notification popup -->
+    <script src="/313devgrp/reference/lightblue4/docs/lib/messenger/build/js/messenger.min.js"></script>
+    <script src="/313devgrp/reference/lightblue4/docs/lib/messenger/build/js/messenger-theme-flat.js"></script>
+
+    <!-- ckeditor4 성능 이슈로 인한 주석 처리. -->
+    <script type="text/javascript" src="/313devgrp/reference/jquery-plugins/ckeditor4-4.22.1/ckeditor.js" charset="utf-8"></script>
+
+    <script type="text/javascript" src="/313devgrp/reference/lightblue4/docs/js/app.js"></script>
+
+    <!-- page interaction -->
+    <script type="text/javascript" src="/313devgrp/reference/jquery-plugins/topbar.js"></script>
+
+    <!-- page js -->
+    <script type="text/javascript" src="/313devgrp/arms/js/common.js" charset="utf-8"></script>
+    <script type="text/javascript" src="/313devgrp/arms/js/common/dwrChat.js" charset="utf-8"></script>
+</head>
+
+<body style="overflow-x: hidden">
+<div
+        class="logo"
+        data-include="html/template/page-logo.html"
+        style="left: 35px !important; position: absolute !important"></div>
+
+<nav
+        id="sidebar"
+        class="sidebar nav-collapse collapse"
+        data-include="html/template/page-sidebar.html"></nav>
+
+<div class="wrap">
+    <header
+            class="page-header"
+            data-include="html/template/page-header.html"></header>
+
+    <div class="content container">
+        <section
+                class="widgetheader"
+                data-include="html/template/content-header.html"></section>
+
+        <div
+                class="row"
+                data-include="html/template/content-container.html"></div>
+
+        <footer
+                class="content-footer pull-right"
+                data-include="html/template/content-footer.html"></footer>
     </div>
-    <?php
-    }
-    ?>
-    <!-- } 최신글 끝 -->
 </div>
 
-<?php
-include_once(G5_THEME_PATH.'/tail.php');
+<div
+        class="loader animated fadeIn darkBack hide"
+        style="z-index: 999999">
+			<span
+                    class="spinner"
+                    style="font-size: 13px !important">
+				<i class="fa fa-empire fa-spin"></i>
+				어플리케이션 API Data를 가져오는 중입니다...
+			</span>
+</div>
+
+<div
+        id="topicon"
+        style="opacity: 1">
+    <img
+            src="/313devgrp/arms/img/topicon.gif"
+            alt="맨위로" />
+</div>
+
+<!-- upload template -->
+<script
+        id="template-upload"
+        type="text/x-tmpl">
+			{% for (var i=0, file; file=o.files[i]; i++) { %}
+			<tr class="template-upload fade">
+			    <td class="preview"><span class="fade"></span></td>
+			    <td class="name"><span>{%=file.name%}</span></td>
+			    <td class="size"><span>{%=o.formatFileSize(file.size)%}</span></td>
+			    {% if (file.error) { %}
+			    <td class="error" colspan="2"><span class="label label-important">Error</span> {%=file.error%}</td>
+			    {% } else if (o.files.valid && !i) { %}
+			    <td>
+			        <div class="progress progress-success progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0">
+			            <div class="bar" style="width:0%;"></div>
+			        </div>
+			    </td>
+			    <td>{% if (!o.options.autoUpload) { %}
+			        <button class="btn btn-primary btn-sm start">
+			            <i class="fa fa-upload"></i>
+			            <span>Start</span>
+			        </button>
+			        {% } %}</td>
+			    {% } else { %}
+			    <td colspan="2"></td>
+			    {% } %}
+			    <td>{% if (!i) { %}
+			        <button class="btn btn-warning btn-sm cancel">
+			            <i class="fa fa-ban"></i>
+			            <span>Cancel</span>
+			        </button>
+			        {% } %}</td>
+			</tr>
+			{% } %}
+		</script>
+
+<!-- The template to display files available for download -->
+<script
+        id="template-download"
+        type="text/x-tmpl">
+			{% for (var i=0, file; file=o.files[i]; i++) { %}
+			<tr class="template-download fade">
+			    {% if (file.error) { %}
+			    <td></td>
+			    <td class="name"><span>{%=file.name%}</span></td>
+			    <td class="size"><span>{%=o.formatFileSize(file.size)%}</span></td>
+			    <td class="error" colspan="2"><span class="label label-important">Error</span> {%=file.error%}</td>
+			    {% } else { %}
+			    <td class="preview">{% if (file.thumbnail_url) { %}
+			        <a href="{%=file.url%}" title="{%=file.name%}" data-gallery="gallery" download="{%=file.name%}"><img src="{%=file.thumbnail_url%}"></a>
+			        {% } %}</td>
+			    <td class="name">
+			        <a href="{%=file.url%}" title="{%=file.name%}" data-gallery="{%=file.thumbnail_url&&'gallery'%}" download="{%=file.name%}">{%=file.name%}</a>
+			    </td>
+			    <td class="size"><span>{%=o.formatFileSize(file.size)%}</span></td>
+			    <td colspan="2"></td>
+			    {% } %}
+			    <td>
+			        <button class="btn btn-danger btn-sm delete" data-type="{%=file.delete_type%}" data-url="{%=file.delete_url%}"{% if (file.delete_with_credentials) { %} data-xhr-fields='{"withCredentials":true}'{% } %}>
+			        <i class="fa fa-trash"></i>
+			        <span>Delete</span>
+			        </button>
+			    </td>
+			</tr>
+			{% } %}
+		</script>
+
+</body>
+</html>
