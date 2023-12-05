05625da8e1fc8c24abf21fe3a12ab94c72e97921
LeeDongMin <admin@313.co.kr>
2023-12-05 19:10:50 +0900

    feat: [ARMSQA-19] #comment :white_check_mark: 톱니바퀴 처리 - 23.12.05 #close #time 1h +review SR @313devops
    [ Frontend-PHP::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    요구 관리 : [Requirement Manage] http://www.a-rms.net
    문서 관리 : [Document] http://www.313.co.kr/confluence
    이슈 관리 : [IssueTracker] http://www.313.co.kr/jira
    소스 관리 : [VersionControl] https://github.com/313devgrp
    코드 리뷰 : [CodeReview] http://www.313.co.kr/fecru
    빌드 관리 : [CICD-Deploy] http://www.313.co.kr/jenkins
    배포 관리 : [Deploy-Pipeline] http://www.313.co.kr/spinnaker
    아티 팩트 : [ArtifactManager] http://www.313.co.kr/nexus
    코드 분석 : [CodeAnalysis] http://www.313.co.kr/sonar
    성능 측정 : [ZipKin] http://www.313.co.kr/zipkin/
    
    도커 관리 : [Docker Hub] https://hub.docker.com/u/313devgrp
    클러스터1 : [Kubernetes] http://www.313.co.kr:31380
    클러스터2 : [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    디비 관리 : [DB Admin] http://www.313.co.kr/phpmyadmin/
    오브 젝트 : [S3 Admin] http://www.313.co.kr/minio/login
    캐시 관리 : [Cache Admin] http://www.313.co.kr/redis/
    ELFK 스택1 : [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    ELFK 스택2 : [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    ELFK 스택3 : [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    ELFK 스택4 : [Kibana] http://www.313.co.kr/kibana/app/home#/
    
    모니터링 1 : [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    모니터링 2 : [Grafana] http://www.313.co.kr/grafana
    
    인증 인가 : [Auth] http://www.313.co.kr/auth/
    
    NFS 관리 : [NAS] http://www.313.co.kr:5000/webman/index.cgi
    메일 관리 : [Mail] http://www.313.co.kr/mail/
    원격 관리 : [RDP] http://www.313.co.kr/guacamole/#/

--- js/index.js
+++ js/index.js
@@ -5,33 +5,53 @@
 function execDocReady() {
 
     var pluginGroups = [
-            [
-                "/313devgrp/reference/light-blue/lib/vendor/jquery.ui.widget.js",
-                "/313devgrp/reference/light-blue/lib/vendor/http_blueimp.github.io_JavaScript-Templates_js_tmpl.js",
-                "/313devgrp/reference/light-blue/lib/vendor/http_blueimp.github.io_JavaScript-Load-Image_js_load-image.js",
-                "/313devgrp/reference/light-blue/lib/vendor/http_blueimp.github.io_JavaScript-Canvas-to-Blob_js_canvas-to-blob.js",
-                "/313devgrp/reference/light-blue/lib/jquery.iframe-transport.js",
-                "/313devgrp/reference/light-blue/lib/jquery.fileupload.js",
-                "/313devgrp/reference/light-blue/lib/jquery.fileupload-fp.js",
-                "/313devgrp/reference/light-blue/lib/jquery.fileupload-ui.js"
-            ],
-            [
-                "/313devgrp/reference/lightblue4/docs/lib/slimScroll/jquery.slimscroll.min.js",
-                "/313devgrp/reference/jquery-plugins/unityping-0.1.0/dist/jquery.unityping.min.js",
-                "/313devgrp/reference/light-blue/lib/bootstrap-datepicker.js",
-                "/313devgrp/reference/jquery-plugins/datetimepicker-2.5.20/build/jquery.datetimepicker.min.css",
-                "/313devgrp/reference/jquery-plugins/datetimepicker-2.5.20/build/jquery.datetimepicker.full.min.js",
-                "/313devgrp/reference/lightblue4/docs/lib/widgster/widgster.js"
-            ],
-            [
-                "/313devgrp/reference/jquery-plugins/select2-4.0.2/dist/css/select2_lightblue4.css",
-                "/313devgrp/reference/jquery-plugins/select2-4.0.2/dist/js/select2.min.js",
-                "/313devgrp/reference/jquery-plugins/lou-multi-select-0.9.12/css/multiselect-lightblue4.css",
-                "/313devgrp/reference/jquery-plugins/lou-multi-select-0.9.12/js/jquery.quicksearch.js",
-                "/313devgrp/reference/jquery-plugins/lou-multi-select-0.9.12/js/jquery.multi-select.js",
-                "/313devgrp/reference/jquery-plugins/multiple-select-1.5.2/dist/multiple-select-bluelight.css",
-                "/313devgrp/reference/jquery-plugins/multiple-select-1.5.2/dist/multiple-select.min.js"
-            ],
+        [
+            "/313devgrp/reference/light-blue/lib/vendor/jquery.ui.widget.js",
+            "/313devgrp/reference/light-blue/lib/vendor/http_blueimp.github.io_JavaScript-Templates_js_tmpl.js",
+            "/313devgrp/reference/light-blue/lib/vendor/http_blueimp.github.io_JavaScript-Load-Image_js_load-image.js",
+            "/313devgrp/reference/light-blue/lib/vendor/http_blueimp.github.io_JavaScript-Canvas-to-Blob_js_canvas-to-blob.js",
+            "/313devgrp/reference/light-blue/lib/jquery.iframe-transport.js",
+            "/313devgrp/reference/light-blue/lib/jquery.fileupload.js",
+            "/313devgrp/reference/light-blue/lib/jquery.fileupload-fp.js",
+            "/313devgrp/reference/light-blue/lib/jquery.fileupload-ui.js"
+        ],
+
+        [
+            "/313devgrp/reference/lightblue4/docs/lib/slimScroll/jquery.slimscroll.min.js",
+            "/313devgrp/reference/jquery-plugins/unityping-0.1.0/dist/jquery.unityping.min.js",
+            "/313devgrp/reference/light-blue/lib/bootstrap-datepicker.js",
+            "/313devgrp/reference/jquery-plugins/datetimepicker-2.5.20/build/jquery.datetimepicker.min.css",
+            "/313devgrp/reference/jquery-plugins/datetimepicker-2.5.20/build/jquery.datetimepicker.full.min.js",
+            "/313devgrp/reference/lightblue4/docs/lib/widgster/widgster.js"
+        ],
+
+        [
+            "/313devgrp/reference/jquery-plugins/select2-4.0.2/dist/css/select2_lightblue4.css",
+            "/313devgrp/reference/jquery-plugins/select2-4.0.2/dist/js/select2.min.js",
+            "/313devgrp/reference/jquery-plugins/lou-multi-select-0.9.12/css/multiselect-lightblue4.css",
+            "/313devgrp/reference/jquery-plugins/lou-multi-select-0.9.12/js/jquery.quicksearch.js",
+            "/313devgrp/reference/jquery-plugins/lou-multi-select-0.9.12/js/jquery.multi-select.js",
+            "/313devgrp/reference/jquery-plugins/multiple-select-1.5.2/dist/multiple-select-bluelight.css",
+            "/313devgrp/reference/jquery-plugins/multiple-select-1.5.2/dist/multiple-select.min.js"
+        ],
+
+        [
+            "/313devgrp/reference/jquery-plugins/jstree-v.pre1.0/_lib/jquery.cookie.js",
+            "/313devgrp/reference/jquery-plugins/jstree-v.pre1.0/_lib/jquery.hotkeys.js",
+            "/313devgrp/reference/jquery-plugins/jstree-v.pre1.0/jquery.jstree.js",
+            "/313devgrp/reference/jquery-plugins/dataTables-1.10.16/media/css/jquery.dataTables_lightblue4.css",
+            "/313devgrp/reference/jquery-plugins/dataTables-1.10.16/extensions/Responsive/css/responsive.dataTables_lightblue4.css",
+            "/313devgrp/reference/jquery-plugins/dataTables-1.10.16/extensions/Select/css/select.dataTables_lightblue4.css",
+            "/313devgrp/reference/jquery-plugins/dataTables-1.10.16/media/js/jquery.dataTables.min.js",
+            "/313devgrp/reference/jquery-plugins/dataTables-1.10.16/extensions/Responsive/js/dataTables.responsive.min.js",
+            "/313devgrp/reference/jquery-plugins/dataTables-1.10.16/extensions/Select/js/dataTables.select.min.js",
+            "/313devgrp/reference/jquery-plugins/dataTables-1.10.16/extensions/RowGroup/js/dataTables.rowsGroup.min.js",
+            "/313devgrp/reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/dataTables.buttons.min.js",
+            "/313devgrp/reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/buttons.html5.js",
+            "/313devgrp/reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/buttons.print.js",
+            "/313devgrp/reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/jszip.min.js",
+            "/313devgrp/reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/pdfmake.min.js"
+        ]
     ];
 
     loadPluginGroupsParallelAndSequential(pluginGroups)
